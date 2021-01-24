005dc33fb895a9210f220d0e928e1d9a787e6692
chimichi004 <chember004@gmail.com>
2021-01-24 21:58:36 +0800

    curve tweens

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,18 @@ Route _onCreateRoute() {
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
       var begin = Offset(0.0, 1.0);
       var end = Offset.zero;
-      var tween = Tween(begin: begin, end: end);
-      var offsetAnimation = animation.drive(tween);
+      var curve = Curves.ease;
 
+      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+      /* Another way to create an Animation<Offset> with an easing curve
+       * is to use a CurvedAnimation: 
+       */
+      // var curvedAnimation = CurvedAnimation(
+      //   parent: animation,
+      //   curve: curve,
+      // );
       return SlideTransition(
-        position: offsetAnimation,
+        position: animation.drive(tween),
         child: child,
       );
     },
