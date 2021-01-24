92f1f6a2614b21deacbd5379cb29a4bfeb34c970
chimichi004 <chember004@gmail.com>
2021-01-24 23:20:53 +0800

    Animate the properties of a container

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-//import './animation/page_route_transition.dart';
-import './animation/physics_simulation.dart';
+//import 'animation/page_route_transition.dart';
+//import 'animation/physics_simulation.dart';
+import 'animation/animate_container.dart';
 
 main() {
   runApp(MaterialApp(
@@ -13,6 +14,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return PhysicsCardDragDemo();
+    return AnimatedContainerApp();
   }
 }
