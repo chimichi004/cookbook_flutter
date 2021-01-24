c9090df7c25b0b7344bf65c9bd6892b00c0c7b5e
chimichi004 <chember004@gmail.com>
2021-01-24 22:58:33 +0800

    Animate a widget using a physics simulation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+//import './animation/page_route_transition.dart';
+import './animation/physics_simulation.dart';
+
 main() {
   runApp(MaterialApp(
     home: MyApp(),
@@ -10,52 +13,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: ElevatedButton(
-          child: Text('Go!'),
-          onPressed: () {
-            Navigator.of(context).push(_onCreateRoute());
-          },
-        ),
-      ),
-    );
-  }
-}
-
-Route _onCreateRoute() {
-  return PageRouteBuilder(
-    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
-    transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      var begin = Offset(0.0, 1.0);
-      var end = Offset.zero;
-      var curve = Curves.ease;
-
-      var tween = Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-      /* Another way to create an Animation<Offset> with an easing curve
-       * is to use a CurvedAnimation: 
-       */
-      // var curvedAnimation = CurvedAnimation(
-      //   parent: animation,
-      //   curve: curve,
-      // );
-      return SlideTransition(
-        position: animation.drive(tween),
-        child: child,
-      );
-    },
-  );
-}
-
-class Page2 extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Text('Page 2'),
-      ),
-    );
+    return PhysicsCardDragDemo();
   }
 }
