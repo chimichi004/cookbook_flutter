51438327ee2f6cc95bbad543962e54697f493920
chimichi004 <chember004@gmail.com>
2021-01-26 21:05:01 +0800

    Add a Drawer to a screen

---
+++ lib/design/add_drawer.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class SampleDrawer extends StatelessWidget {
+  final String title;
+
+  SampleDrawer({this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Center(
+          child: Text('My sample page'),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Drawer Header'),
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                ),
+              ),
+              ListTile(
+                title: Text('Item 1'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              ),
+              ListTile(
+                title: Text('Item 2'),
+                onTap: () {
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
