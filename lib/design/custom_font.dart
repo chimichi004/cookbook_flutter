413f53c34e5e96469998ab2b7f7e71a2c7557e44
chimichi004 <chember004@gmail.com>
2021-01-27 22:56:38 +0800

    Use a custom font

---
+++ lib/design/custom_font.dart
@@ -0,0 +1,24 @@
+// https://flutter.dev/docs/cookbook/design/fonts
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomFont extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // The AppBar uses the app-default Raleway font.
+      appBar: AppBar(title: Text('Custom Fonts')),
+      body: Center(
+        // This Text widget uses the RobotoMono font.
+        child: Text(
+          'Roboto Mono sample',
+          style: GoogleFonts.robotoMono(
+            textStyle: TextStyle(color: Colors.blue, letterSpacing: .5),
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:cookbook_flutter/design/add_snackbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 /*Animations */
 //import 'animation/page_route_transition.dart';
@@ -11,7 +11,8 @@ import 'package:flutter/material.dart';
 //import 'design/add_drawer.dart';
 //import 'design/add_snackbar.dart';
 //import 'design/export_font.dart';
-import 'design/add_orientationbuilder.dart';
+//import 'design/add_orientationbuilder.dart';
+import 'design/custom_font.dart';
 
 main() {
   runApp(MaterialApp(
@@ -20,19 +21,17 @@ main() {
 }
 
 class MyApp extends StatelessWidget {
-  final appTitle = 'Sample Drawer Demo';
+  final appTitle = 'Sample Demo';
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     //  return SampleSnackbar();
     return MaterialApp(
-      title: 'SnackBar Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('SnackBar Demo'),
-        ),
-        body: SampleOrientation(),
+      title: appTitle,
+      theme: ThemeData(
+        textTheme: GoogleFonts.ralewayTextTheme(Theme.of(context).textTheme),
       ),
+      home: CustomFont(),
     );
   }
 }
