6372a84361056d718c8380475b7396cacc43c8d9
chimichi004 <chember004@gmail.com>
2021-01-30 01:10:06 +0800

    https://flutter.dev/docs/cookbook/lists/horizontal-list

---
+++ lib/list/list_horizontal.dart
@@ -0,0 +1,47 @@
+// https://flutter.dev/docs/cookbook/lists/horizontal-list
+
+import 'package:flutter/material.dart';
+
+class HorizontalList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final title = "Demo horiazontal list";
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(title),
+        ),
+        body: Container(
+          margin: EdgeInsets.symmetric(vertical: 20.0),
+          height: 200.0,
+          child: ListView(
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[
+              Container(
+                width: 160.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.blue,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.green,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 160.0,
+                color: Colors.orange,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
