0b5e4694041aa16bc2541f816d81d9d5fb11a745
chimichi004 <chember004@gmail.com>
2021-02-07 18:30:56 +0800

    Navigate with named routes

---
+++ lib/navigation/nav_name_routes/secondscreem.dart
@@ -0,0 +1,24 @@
+// https://flutter.dev/docs/cookbook/navigation/named-routes
+
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second screen'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to the first screen by popping the current route
+            // off the stack.
+            Navigator.pop(context);
+          },
+          child: Text('Go back!!'),
+        ),
+      ),
+    );
+  }
+}
