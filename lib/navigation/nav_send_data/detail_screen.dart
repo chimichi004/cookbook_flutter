c317c8dc73bcdbd490aba097f33beed72bffcb15
chimichi004 <chember004@gmail.com>
2021-02-07 20:54:17 +0800

    Send data to a new screen

---
+++ lib/navigation/nav_send_data/detail_screen.dart
@@ -0,0 +1,26 @@
+// https://flutter.dev/docs/cookbook/navigation/passing-data
+
+import 'package:cookbook_flutter/navigation/nav_send_data/todo.dart';
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  // Declare a field that holds the Todo.
+  final Todo todo;
+
+  // In the constructor, require a Todo.
+  DetailScreen({Key key, @required this.todo}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // Use the Todo to create the UI.
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(todo.title),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Text(todo.description),
+      ),
+    );
+  }
+}
