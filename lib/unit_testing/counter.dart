54394222b74974cb0afcdea09901e75e394d0a6b
chimichi004 <chember004@gmail.com>
2021-02-21 10:47:22 +0800

    Mock dependencies using Mockito

---
+++ lib/unit_testing/counter.dart
@@ -0,0 +1,9 @@
+// https://flutter.dev/docs/cookbook/testing/unit/introduction
+
+class Counter {
+  int value = 0;
+
+  void increment() => value++;
+
+  void decrement() => value--;
+}
---
+++ lib/unit_testing/counter_test.dart
@@ -0,0 +1,29 @@
+// https://flutter.dev/docs/cookbook/testing/unit/introduction
+
+// Import the test package and Counter class
+import 'package:cookbook_flutter/unit_testing/counter.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Counter', () {
+    test('value should start at 0', () {
+      expect(Counter().value, 0);
+    });
+
+    test('value should be incremented', () {
+      final counter = Counter();
+
+      counter.increment();
+
+      expect(counter.value, 1);
+    });
+
+    test('value should be decremented', () {
+      final counter = Counter();
+
+      counter.decrement();
+
+      expect(counter.value, -1);
+    });
+  });
+}
